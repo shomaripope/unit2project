14641c84d95f75d8b79cf5b91c3b6d71e7ac174d
Shomari Pope <shomaripope@gmail.com>
2019-01-11 11:40:04 -0500

    got create read update...working on delete now

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,8 @@
-const mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost/carsApp").then(() => {
+
+const mongoose = require("mongoose")
+require('dotenv').config()
+
+mongoose.connect(process.env.MONGODB_URI).then(() => {
     console.log("MONGODB is now connected")
 })
 
